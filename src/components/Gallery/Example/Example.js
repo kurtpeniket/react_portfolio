aade8a2bff12a1fa64c6463444fc71d7907060b7
Kurt Peniket <kurt.peniket@gmail.com>
2021-04-27 17:22:33 +0100

    added styled components for example dynamic css

--- src/components/Gallery/Example/Example.js
+++ src/components/Gallery/Example/Example.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './Example.css';
 import ReactCardFlip from 'react-card-flip';
+import styled from 'styled-components';
 
 class Example extends React.Component {
   constructor() {
@@ -18,13 +18,38 @@ class Example extends React.Component {
 
   render() {
 
-    const style = {
-      backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${this.props.img})`
+    const Styles = styled.div`
+    .card {
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: center;
+      height: 280px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      font-size: 24px;
+      font-weight: bold;
+      text-shadow: 1px 1px 3px rgba(0,0,0,0.2);
+      border-radius: 8px;
+      box-shadow: 0 0 15px rgba(0,0,0,0.2);
+      background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${props => props.img})
     }
+  
+    .card p {
+      padding: 30px;
+      font-size: 16px;
+    }
+  
+    .card, a:link {
+        text-decoration: none;
+    }
+    `
 
     return (
       <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-          <div style={style} onMouseEnter={this.handleClick}>
+        <Styles img={this.props.img}>
+          <div onMouseEnter={this.handleClick}>
             <a href={this.props.link}>
               <div className={'card'}>
                 {this.props.children}
@@ -32,8 +57,10 @@ class Example extends React.Component {
               </div>
             </a>
           </div>
+        </Styles>
 
-          <div style={style} onMouseLeave={this.handleClick}>
+        <Styles>
+          <div onMouseLeave={this.handleClick}>
             <a href={this.props.link}>
               <div className={'card'}>
                 {this.props.children}
@@ -41,23 +68,10 @@ class Example extends React.Component {
               </div>
             </a>
           </div>
+        </Styles>
       </ReactCardFlip>
     )
   }
 }
-
-// const Example = (props) => {
-
-//   return (
-//     <div style={style}>
-//       <a href={props.link}>
-//         <div className={'card'}>
-//           {props.children}
-//           Breakfast
-//         </div>
-//       </a>
-//     </div>
-//   )
-// };
 
 export default Example;
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Example from './Example/Example';
+import Paragraph from '../Paragraph/Paragraph';
 import './Gallery.css'
 
 const Gallery = (props) => {
   return (
     <div className={'cards'}>
       <Example
-        link='https://uikit.lewagon.com/documentation#card_category' 
-        img='https://source.unsplash.com/random' 
-        front='This is the front'
-        back='This is the back'
+        link='https://www.illuminate-app.tech/' 
+        img='https://i.ibb.co/QfXWytN/illuminate1.png' 
+        front='Illuminate'
+        back={<Paragraph text='A full stack mobile focused web app for finding the right light bulb, using RoR, Bootstrap and JavaScript.'/>}
       />
 
-      <Example />
+      <Example img='https://source.unsplash.com/random' />
       <Example />
     </div>
   )
